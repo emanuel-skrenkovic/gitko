0877f803848ec8bb41354630f6b172d1a62161da
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2021-12-26 13:42:38 +0100

    added commit diff window

---
+++ src/gitko/commit_diff_window.rs
@@ -0,0 +1,57 @@
+use crate::render::window::Window;
+use crate::render::display::Display;
+use crate::render::window::ScreenSize;
+
+use crate::git::commands as git;
+
+pub struct CommitDiffWindow {
+    commit_hash: String,
+    data_start: usize,
+    data: Vec<String>,
+    display: Display
+}
+
+impl CommitDiffWindow {
+    pub fn new(size: ScreenSize, commit_hash: &str) -> CommitDiffWindow {
+        CommitDiffWindow {
+            commit_hash: commit_hash.to_string(),
+            data_start: 0,
+            data: vec![],
+            display: Display::new(size)
+        }
+    }
+}
+
+impl Window for CommitDiffWindow {
+    fn on_activate(&mut self) {
+        self.data = git::diff_commit(&self.commit_hash);
+
+        self.display.queue_write_buffer(&self.data);
+    }
+
+    // TODO: Passthrough methods are evil!
+    // Think of a better way.
+
+    fn display(&self) -> &Display { &self.display }
+    fn display_mut(&mut self) -> &mut Display { &mut self.display }
+
+    fn data(&self) -> &Vec<String> {
+        &self.data
+    }
+
+    fn start_position(&self) -> usize {
+        self.data_start
+    }
+
+    fn set_start_position(&mut self, new_position: usize) {
+        self.data_start = new_position;
+    }
+
+    fn close(&self) {
+        self.display.close();
+    }
+
+    fn clear(&self) {
+        self.display.clear();
+    }
+}
--- src/gitko/log_window.rs
+++ src/gitko/log_window.rs
@@ -3,6 +3,10 @@ use crate::render::window::Window;
 use crate::render::display::Display;
 use crate::render::window::ScreenSize;
 
+use crate::render::ascii_table::*;
+
+use crate::gitko::commit_diff_window::CommitDiffWindow;
+
 pub struct LogWindow {
     data: Vec<String>,
     display: Display,
@@ -23,6 +27,22 @@ impl Window for LogWindow {
     fn on_activate(&mut self) {
         self.data = git::log(None);
         self.display.queue_write_buffer(&self.data);
+    }
+
+    fn on_keypress(&mut self, c: i32) -> bool {
+        match c {
+            KEY_LF => {
+                let line = self.display.get_cursor_line_data();
+                let trimmed_line = line
+                    .trim_matches(|c| c == '|' || c == '\\' || c == '*' || c == ' ');
+                let commit_hash = &trimmed_line[0..7];
+
+                self.render_child(CommitDiffWindow::new(ScreenSize::max(), commit_hash));
+            }
+            _ => {}
+        }
+
+        true
     }
 
     fn data(&self) -> &Vec<String> { &self.data }
