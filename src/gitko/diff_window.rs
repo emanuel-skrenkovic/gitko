1cf10fc0cd320e3479d7e36e1121d314a5626dcf
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2022-06-11 20:56:09 +0200

    added search to diff window

--- src/gitko/diff_window.rs
+++ src/gitko/diff_window.rs
@@ -4,16 +4,22 @@ use std::{fs::File, io::{BufReader, BufRead}};
 use crate::git;
 use crate::git::{FileState};
 use crate::ascii_table::*;
-use crate::render::{Colored, Component, Line, Window};
+use crate::gitko::input_window::InputWindow;
+use crate::render::{Renderer, ScreenSize, Position, Colored, Component, Line, Window};
 
 pub struct DiffWindow {
     path: String,
-    file_state: FileState
+    file_state: FileState,
+    term: String
 }
 
 impl DiffWindow {
     pub fn new(path: &str, file_state: FileState) -> DiffWindow {
-        DiffWindow { path: path.to_string(), file_state }
+        DiffWindow {
+            path: path.to_string(),
+            file_state,
+            term: "".to_owned()
+        }
     }
 
     fn move_screen_up(&mut self, window: &mut Window) -> bool {
@@ -40,6 +46,36 @@ impl DiffWindow {
         }
 
         true
+    }
+
+    fn search_diff(&mut self, window: &mut Window) -> bool {
+        self.clear_search();
+        let mut search_window = InputWindow::new();
+
+        Renderer::new(
+            &mut search_window,
+            ScreenSize { lines: 2, cols: window.width() },
+            Position { x: 0, y: window.height() - 2 }
+        ).render();
+
+        self.term = search_window.text;
+        window.move_next(&self.term);
+
+        true
+    }
+
+    fn next_search_result(&mut self, window: &mut Window) -> bool {
+        window.move_next(&self.term);
+        true
+    }
+
+    fn prev_search_result(&mut self, window: &mut Window) -> bool {
+        window.move_prev(&self.term);
+        true
+    }
+
+    fn clear_search(&mut self) {
+        self.term = "".to_owned();
     }
 }
 
@@ -108,8 +144,13 @@ impl Component<DiffWindow> for DiffWindow {
     fn register_handlers(&self, handlers: &mut HashMap<i32, fn(&mut DiffWindow, &mut Window) -> bool>) {
         handlers.insert(KEY_J_LOWER, DiffWindow::move_screen_down);
         handlers.insert(KEY_K_LOWER, DiffWindow::move_screen_up);
+
         handlers.insert(4, DiffWindow::jump_screen_down);
         handlers.insert(21, DiffWindow::jump_screen_up);
+
+        handlers.insert(KEY_N_LOWER, DiffWindow::next_search_result);
+        handlers.insert(KEY_N_UPPER, DiffWindow::prev_search_result);
+        handlers.insert(KEY_FORWARD_SLASH, DiffWindow::search_diff);
     }
 }
 
