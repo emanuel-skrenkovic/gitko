855267e5824f144f4dc660151364b57d2845d4ea
Emanuel Skrenković <eskrenkovic@gmail.com>
2024-01-16 21:21:16 +0100

    renderers as separate features

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,83 @@
-use gitko_crossterm::{init, exit, screen_factory, MAX_HEIGHT, MAX_WIDTH};
 use gitko_render::{Renderer, ScreenSize, Position, ScreenFactory};
-
-use crate::gitko::main_window::MainWindow;
 
 mod git;
 mod gitko;
 mod searchable;
 
-#[allow(dead_code)]
+fn main() {
+    #[allow(dead_code)]
+    #[cfg(feature = "ncurses")] {
+        use gitko_ncurses_render::{init, exit};
+        use crate::gitko::main_window::MainWindow;
+
+        init();
+
+        Renderer::new(
+            &mut MainWindow::new(),
+            ScreenSize::max(),
+            Position::default(),
+            screen()
+        ).render();
+
+        exit();
+    }
+
+    #[allow(dead_code)]
+    #[cfg(feature = "crossterm")] {
+        use gitko_crossterm::{init, exit};
+        use crate::gitko::main_window::MainWindow;
+
+        init();
+
+        Renderer::new(
+            &mut MainWindow::new(),
+            ScreenSize::max(),
+            Position::default(),
+            screen()
+        ).render();
+
+        exit();
+    }
+}
+
 fn max_width() -> i32 {
-    unsafe { MAX_WIDTH }
+    #[allow(dead_code)]
+    #[cfg(feature = "ncurses")] {
+        use gitko_ncurses_render::MAX_WIDTH;
+        unsafe { return MAX_WIDTH }
+    }
+    #[allow(dead_code)]
+    #[cfg(feature = "crossterm")] {
+        use gitko_crossterm::MAX_WIDTH;
+        unsafe { MAX_WIDTH }
+    }
 }
 
 #[allow(dead_code)]
 fn max_height() -> i32 {
-    unsafe { MAX_HEIGHT }
+    #[allow(dead_code)]
+    #[cfg(feature = "ncurses")] {
+        use gitko_ncurses_render::MAX_HEIGHT;
+        unsafe { return MAX_HEIGHT }
+    }
+
+    #[allow(dead_code)]
+    #[cfg(feature = "crossterm")] {
+        use gitko_crossterm::MAX_HEIGHT;
+        unsafe { MAX_HEIGHT }
+    }
 }
 
 fn screen() -> ScreenFactory {
-    screen_factory
+    #[allow(dead_code)]
+    #[cfg(feature = "ncurses")] {
+        use gitko_ncurses_render::screen_factory;
+        return screen_factory
+    }
+
+    #[allow(dead_code)]
+    #[cfg(feature = "crossterm")] {
+        use gitko_crossterm::screen_factory;
+        screen_factory
+    }
 }
-
-fn main() {
-    init();
-
-    Renderer::new(
-        &mut MainWindow::new(),
-        ScreenSize::max(),
-        Position::default(),
-        screen()
-    ).render();
-
-    exit();
-}
-
-
